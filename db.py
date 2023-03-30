959606c1c011d6a98b87a49e040bd08151b16c20
Evgeny Grakovich <grakovich-e@yandex.by>
2023-03-30 09:58:13 +0300

    init

---
+++ db.py
@@ -0,0 +1,27 @@
+import psycopg2
+import os
+
+
+class Database(object):
+    def __init__(self):
+        self.conn = psycopg2.connect(
+            database=str(os.environ.get('database')),
+            user=str(os.environ.get('user')),
+            password=str(os.environ.get('password')),
+            host=str(os.environ.get('host')),
+            port=str(os.environ.get('port'))
+        )
+        self.curs = self.conn.cursor()
+
+    def __enter__(self):
+        return self.curs
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.conn.commit()
+        self.conn.close()
+
+def add_film(uuid, title, code, has_oscar, photo, year, country, director, actors, rating, genre, about):
+    with Database() as curs:
+        _SQL = f"""insert into robot_movie values
+        ($${uuid}$$, $${title}$$, {code}, {has_oscar}, $${photo}$$, {year}, $${country}$$, $${director}$$, $${actors}$$, {rating}, $${genre}$$, $${about}$$);"""
+        curs.execute(_SQL)
---
+++ test.py
@@ -0,0 +1,5 @@
+import db
+import uuid 
+import main
+
+print(main.get_oscar(8124))
